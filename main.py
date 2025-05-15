76ec3479632c577a1ea1449b3e49adb876b233be
quietpulse <amanme9285@gmail.com>
2025-05-16 00:44:32 +0530

    refactor: use getoutput for Core0 temp with loop in one line out

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world.")
+import time
+import subprocess
+
+def core_temp(core):
+    pattern = "\'\\d+\\.\\d+°C\'"
+    cmd = f"sensors | grep \'Core {core}\' | grep -oP {pattern} | head -n 1"
+    return subprocess.getoutput(cmd)
+
+i = 1
+a = 0
+
+while a <= i :
+    print(f'\r{core_temp(0)}', end="", flush=True)
+    time.sleep(1)
