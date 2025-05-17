cf7a35b479ab600f876efa1908a25bace1fe296f
quietpulse <amanme9285@gmail.com>
2025-05-17 22:29:27 +0530

    refactor: using psutil lib for meagering cpu temp.

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import time
-import subprocess
+import os
+import psutil
 
-def core_temp(core):
-    pattern = "\'\\d+\\.\\d+°C\'"
-    cmd = f"sensors | grep \'Core {core}\' | grep -oP {pattern} | head -n 1"
-    return subprocess.getoutput(cmd)
+total_core = os.cpu_count() #total cpu core count
 
-i = 1
-a = 0
+#get a cpu core temp with lm_sensors
+        
+def locate_temp():
+    temps = psutil.sensors_temperatures()
+    cpu_pkg = temps['coretemp'][0].current
+    return int(cpu_pkg)
 
-while a <= i :
-    print(f'\rYour Core 0\'s temp is {core_temp(0)}', end="", flush=True)
+terminal_size = os.get_terminal_size() # get term size
+
+for i in range(0, 999999999):
+    os.system('cls' if os.name == 'nt' else 'clear') #clear the terminal
+
+    cpu_temp = locate_temp()
+
+    base_str = f'Your CPU\'s temp is {cpu_temp}°C'
+    print(base_str.center(int(terminal_size.columns),), end="")
+
     time.sleep(1)
